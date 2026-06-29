5b5f523b34d1aae8035d887410c4f820be4a73b2
wzx <1017293270@qq.com>
2026-06-29 18:19:10 +0900

    practice: use noninteractive matplotlib backend

--- practice/01-factor-modeling-lab/app.py
+++ practice/01-factor-modeling-lab/app.py
@@ -6,6 +6,9 @@ import argparse
 from dataclasses import dataclass
 
 import gradio as gr
+import matplotlib
+
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
